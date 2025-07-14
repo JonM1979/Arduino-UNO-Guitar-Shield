ae1957c9430b1b91f1d550933688cf9e918ba695
Jonathan Melgar <amelgar08@gmail.com>
2025-07-14 07:22:37 -0400

    shield_distortion.ino

--- shield_distortion.ino
+++ shield_distortion.ino
@@ -82,29 +82,32 @@ void setup()
 void loop() 
 {
   //  Turn on the LED if the effect is ON.
-  if (digitalRead(FOOTSWITCH)) digitalWrite(LED, HIGH); 
-    else  digitalWrite(LED, LOW);
-  //nothing here, all happens in the Timer 1 interruption.
+  if (digitalRead(FOOTSWITCH)) {
+    digitalWrite(LED, HIGH); 
 
-  // Scroll full screen
- display.setCursor(0, 0);
- display.setTextSize(1);
- display.println("Distortion!");
- display.display();
- display.startscrollright(0x00, 0x07);
- delay(4500);
- display.stopscroll();
- delay(1000);
- display.startscrollleft(0x00, 0x07);
- delay(4500);
- display.stopscroll();
- delay(1000);
- display.startscrolldiagright(0x00, 0x07);
- delay(4500);
- display.startscrolldiagleft(0x00, 0x07);
- delay(4500);
- display.stopscroll();
- display.clearDisplay();
+    // Scroll full screen
+   display.setCursor(0, 0);
+   display.setTextSize(1);
+   display.println("Distortion!");
+   display.display();
+   display.startscrollright(0x00, 0x07);
+   delay(4500);
+   display.stopscroll();
+   delay(1000);
+   display.startscrollleft(0x00, 0x07);
+   delay(4500);
+   display.stopscroll();
+   delay(1000);
+   display.startscrolldiagright(0x00, 0x07);
+   delay(4500);
+   display.startscrolldiagleft(0x00, 0x07);
+   delay(4500);
+   display.stopscroll();
+   display.clearDisplay();
+  }
+    else  {
+      digitalWrite(LED, LOW); }
+  //nothing here, all happens in the Timer 1 interruption.
 
 }
 
