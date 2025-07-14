9638821d4f79788248f3f51cf8ddeaf01a225601
Jonathan Melgar <amelgar08@gmail.com>
2025-07-14 07:33:14 -0400

    shield_signal_generator.ino

--- shield_signal_generator.ino
+++ shield_signal_generator.ino
@@ -145,31 +145,33 @@ void setup()
 void loop() 
 {
   //Turn on the LED if the effect is ON.
-  if (digitalRead(FOOTSWITCH)) digitalWrite(LED, HIGH); 
-    else  digitalWrite(LED, LOW);
+  if (digitalRead(FOOTSWITCH)) {
+      digitalWrite(LED, HIGH);
+  
+      // Scroll full screen
+     display.setCursor(0, 0);
+     display.setTextSize(1);
+     display.println("Signal Generator!");
+     display.display();
+     display.startscrollright(0x00, 0x07);
+     delay(4500);
+     display.stopscroll();
+     delay(1000);
+     display.startscrollleft(0x00, 0x07);
+     delay(4500);
+     display.stopscroll();
+     delay(1000);
+     display.startscrolldiagright(0x00, 0x07);
+     delay(4500);
+     display.startscrolldiagleft(0x00, 0x07);
+     delay(4500);
+     display.stopscroll();
+     display.clearDisplay();
+  }
+  else { 
+    digitalWrite(LED, LOW); }
   
   //nothing else here, all happens in the Timer 1 interruption.
-
-  // Scroll full screen
- display.setCursor(0, 0);
- display.setTextSize(1);
- display.println("Signal Generator!");
- display.display();
- display.startscrollright(0x00, 0x07);
- delay(4500);
- display.stopscroll();
- delay(1000);
- display.startscrollleft(0x00, 0x07);
- delay(4500);
- display.stopscroll();
- delay(1000);
- display.startscrolldiagright(0x00, 0x07);
- delay(4500);
- display.startscrolldiagleft(0x00, 0x07);
- delay(4500);
- display.stopscroll();
- display.clearDisplay();
-  
 }
 
 ISR(TIMER1_CAPT_vect) 
