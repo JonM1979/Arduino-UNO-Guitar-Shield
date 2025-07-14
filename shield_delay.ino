dabb0b2bf546fcedd19db945b797c06fc2c0f157
Jonathan Melgar <amelgar08@gmail.com>
2025-07-14 07:29:19 -0400

    shield_delay.ino

--- shield_delay.ino
+++ shield_delay.ino
@@ -83,30 +83,32 @@ void setup()
 void loop() 
 {
     //Turn on the LED if the effect is ON.
-  if (digitalRead(FOOTSWITCH)) digitalWrite(LED, HIGH); 
-     else  digitalWrite(LED, LOW);
-  //nothing here, all happens in the Timer 1 interruption.
-
-  // Scroll full screen
- display.setCursor(0, 0);
- display.setTextSize(1);
- display.println("Delay!");
- display.display();
- display.startscrollright(0x00, 0x07);
- delay(4500);
- display.stopscroll();
- delay(1000);
- display.startscrollleft(0x00, 0x07);
- delay(4500);
- display.stopscroll();
- delay(1000);
- display.startscrolldiagright(0x00, 0x07);
- delay(4500);
- display.startscrolldiagleft(0x00, 0x07);
- delay(4500);
- display.stopscroll();
- display.clearDisplay();
+  if (digitalRead(FOOTSWITCH)) { 
+      digitalWrite(LED, HIGH); 
   
+      // Scroll full screen
+     display.setCursor(0, 0);
+     display.setTextSize(1);
+     display.println("Delay!");
+     display.display();
+     display.startscrollright(0x00, 0x07);
+     delay(4500);
+     display.stopscroll();
+     delay(1000);
+     display.startscrollleft(0x00, 0x07);
+     delay(4500);
+     display.stopscroll();
+     delay(1000);
+     display.startscrolldiagright(0x00, 0x07);
+     delay(4500);
+     display.startscrolldiagleft(0x00, 0x07);
+     delay(4500);
+     display.stopscroll();
+     display.clearDisplay();
+  }
+  else { 
+    digitalWrite(LED, LOW); }
+  //nothing here, all happens in the Timer 1 interruption.
 }
 
 ISR(TIMER1_CAPT_vect) 
